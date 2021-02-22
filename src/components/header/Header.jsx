1edd61e4e2e394e84b84040a1d278fdec67a035e
AlexMogwai <alexmogwaiii@gmail.com>
2021-02-22 15:25:00 +0200

    initial

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from './../../img/logo.svg'
+
+import './header.scss';
+
+export const Header = () => (
+  <header className="header">
+    <div className="header__container">
+      <img src={logo} alt="logo" className="header__logo"/>
+      <h1 className="header__title">
+        Everybody Gets
+        <div className="header__next-line">
+          Rewarded
+        </div>
+      </h1>
+      <p className="header__text">
+        Turn your everyday customer into a loyal one.
+        It’s never been easier.
+      </p>
+    </div>
+  </header>
+);
